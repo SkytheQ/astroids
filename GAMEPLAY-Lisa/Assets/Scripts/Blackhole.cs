5ec97dcfbf7695716dd5df25062a19e963cb8722
SkytheQ <lieskuh11@hotmail.com>
2016-05-28 19:24:44 +0200

    astroids

---
+++ GAMEPLAY-Lisa/Assets/Scripts/Blackhole.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blackhole : MonoBehaviour
+{
+
+	public Transform PullObject;
+    public float ForceSpeed;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+		PullObject = GetComponent<Transform> ();
+		//print (PullObject);
+    }
+
+	public void OnTriggerStay2D(Collider2D other)
+	{
+
+		if (other.CompareTag("Player"))
+		{
+                if (PullObject != null) {
+                    Debug.Log(PullObject);
+                PullObject = other.transform;
+                    PullObject.transform.position = Vector2.MoveTowards(PullObject.transform.position, this.transform.position, ForceSpeed * Time.deltaTime);
+                }
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
---
+++ GAMEPLAY-Lisa/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	//FOR THE EXPLOSION ANiMATION!!!!
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	private GameController gameController;
+	public int scoreValue;
+
+	void Start(){
+		GameObject gameControllerObj = GameObject.FindWithTag ("GameController");
+		if (gameControllerObj != null) {
+			gameController = gameControllerObj.GetComponent<GameController> ();
+		}
+		if (gameControllerObj == null) {
+			Debug.Log ("Cannot find the 'GameController' in the script");
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		//Dont destroy the boundary!
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy") || other.CompareTag("Asteroid") || other.CompareTag("VoidCol")) {
+			return;	
+		}
+		if (explosion != null) {
+			//FOR THE EXPLOSION ANiMATION!!!!
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+		//Kill the PLAYER!
+		if (other.CompareTag ("Player")) {
+			//INSTANTIATE 2 ASTEROIDS
+			Instantiate (playerExplosion, transform.position, transform.rotation);
+			gameController.GameOver ();
+		}
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
